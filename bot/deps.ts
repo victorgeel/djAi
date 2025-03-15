f32228845d1c6724e74c039665faf4008287178d
Victor is Geek <92975790+victorgeel@users.noreply.github.com>
2025-03-15 16:27:54 +0630

    Update and rename deps.ts to bot/deps.ts

---
+++ bot/deps.ts
@@ -0,0 +1,2 @@
+export { Bot } from "https://deno.land/x/grammy/mod.ts";
+export { config } from "https://deno.land/x/dotenv/mod.ts";
--- deps.ts
+++
@@ -1,4 +0,0 @@
-export { Bot, webhookCallback } from "https://deno.land/x/grammy@1.18.0/mod.ts";
-export { serve } from "https://deno.land/std@0.196.0/http/server.ts";
-export * as dotenv from "https://deno.land/std@0.196.0/dotenv/mod.ts";
-export { fetch } from "https://deno.land/x/fetch@v1.0.0/mod.ts";
